7f0fb00670e189d2f226c97bea55b3643c05ea1c
John Morris <roxenthndr@gmail.com>
2022-03-24 11:18:22 -0500

    Started Chapter 4

---
+++ Chapter4/app.js
@@ -0,0 +1,23 @@
+$(function(){
+    $("ul").on("click", "li", function() {
+        $(this).hide();
+    });
+    
+    $('ul').mousedown(function(event) {
+        switch (event.which) {
+            case 1:
+                alert('Left Mouse button pressed.');
+                break;
+            case 2:
+                alert('Middle Mouse button pressed.');
+                break;
+            case 3:
+                alert('Right Mouse button pressed.');
+                break;
+            default:
+                alert('You have a strange Mouse!');
+        }
+    });
+
+
+});
